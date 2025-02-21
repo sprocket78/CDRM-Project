457165139c0576fa5e9e391e03c57a5aae494a1b
Pari Malam <apid9755@gmail.com>
2025-02-22 04:06:27 +0800

    ignore some protobuf error

--- react/vite.config.js
+++ react/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: false, // Disable source maps to avoid missing .map file errors
+  },
+  server: {
+    sourcemapIgnoreList: () => true, // Ignore missing source maps
+  }
 })
